6abb1ca80304c68d70b186ffec2a185028e914aa
Zach Schuermann <zachary.zvs@gmail.com>
2019-01-19 18:46:33 -0600

    api setup

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from data_loader import update_database
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Ping for /api/v0/ api endpoint'
+    return 'Ping <a href="http://{}:5050/api/v0/">/api/v0/</a> for api'.format(str(request.remote_addr))
 
-@app.route('/api/v0/')
+@app.route('/api/v0/', methods=['GET'])
 def api():
     return jsonify({'message': 'You have reached api root endpoint'})
+
+@app.route('/api/v0/<int:number>', methods=['GET'])
+def hi_number(number):
+    return jsonify({'your number': number})
 
 if __name__ == '__main__':
     print("Updating database...")
