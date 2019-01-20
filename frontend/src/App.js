f25b143a7f290a04f38a6bf900a2b841a054d85c
Zach Schuermann <zachary.zvs@gmail.com>
2019-01-19 21:27:40 -0600

    lil changes to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -21,7 +22,18 @@ class App extends Component {
           >
             About
           </a>
-        </header>
+       <LineChart width={600} height={300} data={data}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+
+         </header>
       </div>
     );
   }
