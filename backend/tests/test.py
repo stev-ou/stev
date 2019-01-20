8f495d290dfae88cbff3bd1e80b681ccc26b3d10
Zach Schuermann <zachary.zvs@gmail.com>
2019-01-19 21:32:10 -0600

    fix

--- backend/tests/test.py
+++ backend/tests/test.py
@@ -4,7 +4,7 @@ class basictest(unittest.TestCase):
     """ Basic tests """
 
     def test_dummy(self):
-        return self.assertEqual(True, False)
+        return self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
