98f3103071ec8e5e6d7c2bcaae9ef8b74fe93731
Sam Jett <samjett247@gmail.com>
2019-01-23 21:11:32 -0600

    Built api schema and added the uuid field to the aggregated db

---
+++ backend/schema.py
@@ -0,0 +1,8 @@
+Schema relative_dept_rating_figure:
+
+{'result':{'course_ranking': 0, 
+			'dept':{'dept_name': 'str', 'courses_in_dept': 0 , 'dept_mean': 0.0, 'dept_sd': 0.0}, 
+			'current_course_mean': 0.0,
+			'Instructors':[{'Instructor_Name': 'str', 'Instructor_mean_in_course': 0.0}, {'Instructor_Name': 'str', 'Instructor_mean_in_course': 0.0}, {...}]
+			}
+			}
