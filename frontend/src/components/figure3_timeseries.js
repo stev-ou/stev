45d4cf032695142639c84d8047482872ed330f74
Sam Jett <samjett247@gmail.com>
2019-03-03 10:29:53 -0600

    Formatted y axis for timeseries chart

--- frontend/src/components/figure3_timeseries.js
+++ frontend/src/components/figure3_timeseries.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   LineChart,
   Line,
+  Label,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -56,37 +57,45 @@ class Fig3 extends React.Component {
       var all_course_ratings = result['course over time']['ratings']
       var all_dept_ratings = result['dept over time']['ratings']
 
+      // Capture all ratings for computing the ylim domain
+      var all_ratings = []
+
       // Loop through all semesters
       for (var i=0; i<all_semesters.length; i++) {
         var current_sem = {}
         current_sem['sem']= all_semesters[i]
         current_sem[result['course over time']['course name']]= all_course_ratings[i]
+        all_ratings.push(all_course_ratings[i])
         current_sem[result['dept over time']['dept name']]= all_dept_ratings[i]
-
+        all_ratings.push(all_dept_ratings[i])
         // Loop through all instructors
         for (var j=0; j<result['instructors'].length; j++) {
           var instr = result['instructors'][j]
-          instr['semesters'].indexOf(all_semesters[i])
 
           if (instr['semesters'].indexOf(all_semesters[i])>=0) {
             current_sem[instr['name']] = instr['ratings'][instr['semesters'].indexOf(all_semesters[i])]
+            all_ratings.push(current_sem[instr['name']])
           }
         }
         // Add current sem to data
         data.push(current_sem)
       }
+
       // Create the lines for each of our datapoints
       var instructors =  []
       for (var k=0; k<result['instructors'].length; k++) {
         instructors.push([k])
       }
 
+      // Define the domain
+      var domain = [Math.floor(Math.min(all_ratings)), Math.ceil(Math.max(all_ratings))]
+
       // Define the lines used to define the style to plot each instructor
       var Lines = instructors.map((l) => {
         return (<Line type='monotone' dataKey={result['instructors'][l[0]]['name']} strokeWidth={4} stroke={colors[l[0]+3]}/>)
       })
 
-      const myTimeSeries = TimeSeriesChart({data:data, cname:result['course over time']['course name'], dname:result['dept over time']['dept name'], colors:colors, lines:Lines});
+      const myTimeSeries = TimeSeriesChart({data:data, cname:result['course over time']['course name'], dname:result['dept over time']['dept name'], colors:colors, domain:domain, lines:Lines});
 
       return(
       myTimeSeries
@@ -95,20 +104,29 @@ class Fig3 extends React.Component {
 
 
 const TimeSeriesChart = props => (
-  <div className="recharts-wrapper" style={{padding:'1em', align: 'center'}}>
-  <ResponsiveContainer width="90%" height={500} style={{display:'inline-block', align:'center'}}>
+  <div className="recharts-wrapper" style={{padding:'0em', align: 'center'}}>
+  <ResponsiveContainer width="90%" height={600} style={{display:'inline-block', align:'center'}}>
     <LineChart
       // width={800}
       // height={400}
       data={props.data}
-      // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      margin={{ top: 30, right: 30, left: 30, bottom: 30}}
       style={{display: "inline-block"}}
     >
-      <XAxis dataKey="sem" padding={{ left: 30, right: 30 }} />
-      <YAxis />
+      <Legend verticalAlign="top" wrapperStyle={{padding: "1.25em"}}/>
+      <XAxis verticalAlign="bottom" dataKey="sem" padding={{ left: 30, right: 30 }}>
+      <Label position='bottom' style={{ textAnchor: 'middle', padding:'0em', fontSize: 20}}>
+        {"Semester of Interest"}
+          </Label>
+      </XAxis>
+      <YAxis domain={props.domain} >
+        <Label angle={270} position='left' style={{ textAnchor: 'middle', fontSize: 20}}>
+        {"Rating (1 to 5, " + props.domain[0].toString() + '-' +props.domain[1].toString() + ' shown)'}
+          </Label>
+      </YAxis>
       <CartesianGrid strokeDasharray="3 3" />
       <Tooltip />
-      <Legend />
+      
       <Line type="monotone" dataKey={props.cname} strokeWidth={12} stroke={props.colors[0]}/>
       <Line type="monotone" dataKey={props.dname} strokeWidth={9} stroke={props.colors[1]} /> 
       {props.lines}
