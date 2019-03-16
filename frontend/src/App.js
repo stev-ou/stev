e06560e91fcc18e3596aa2ef5240da254fb2f3d1
Sam Jett <samjett247@gmail.com>
2019-03-16 10:46:36 -0500

    forgot to put header and landing back in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 import Landing from './components/landing.js';
 import Header from './components/Header.js';
-// import DataView from './components/DataView.js'
-//import Footer from './components/Footer.js';
-import Fig4 from './components/figure4_table_bar_chart.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return (
