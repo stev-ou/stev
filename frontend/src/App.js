8f896f975d1485c123ed2aa6303af8f6be418d02
Sam Jett <samjett247@gmail.com>
2019-03-26 19:28:56 -0500

    Added search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Landing from './components/landing.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import SearchInput from './components/SearchInput.js'
+import SearchForm from './components/SearchForm.js'
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Landing />
+      {/*<Header />
+      <Landing />*/}
+      <SearchForm />
+
     </div>
   );
 };
