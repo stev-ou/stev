6231983bb6a75884e9b2e9746db929c1d13db11c
Zach Schuermann <zachary.zvs@gmail.com>
2019-03-28 10:02:51 -0500

    pretty

--- frontend/src/components/DataView.js
+++ frontend/src/components/DataView.js
@@ -10,7 +10,7 @@ import InstructorFig3TableBar from './instructor/InstructorFig3TableBar.js';
 
 const DataView = props => {
   const uuid = props.search_text;
-    const search_type = props.search_type;
+  const search_type = props.search_type;
 
   if (search_type === 'COURSE') {
     return (
--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -54,7 +54,7 @@ class SearchForm extends React.Component {
     }
   }
 
-    render() {
+  render() {
     // eslint-disable-next-line
     var prompt;
     if (this.props.search_type === SearchType.COURSE) {
@@ -67,12 +67,12 @@ class SearchForm extends React.Component {
         <div className="col-lg-12">
           <form className="validate" onSubmit={this.handleSubmit}>
             {/*<div className="input-group mb-3">*/}
-              <SearchAutocomplete
-                className="w-100"
-                value={this.state.search_text}
-                onChange={this.handleInputChange}
-                search_type={this.props.search_type}
-              />
+            <SearchAutocomplete
+              className="w-100"
+              value={this.state.search_text}
+              onChange={this.handleInputChange}
+              search_type={this.props.search_type}
+            />
             {/*</div>*/}
 
             <div onChange={this.changeRadio.bind(this)}>
