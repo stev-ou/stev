29edb9f81a4b055ecbc4db55d180e9c5f65908c6
Zach Schuermann <zachary.zvs@gmail.com>
2019-03-28 11:33:59 -0500

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@ echo $DOCKER_PW | base64 --decode -i > ${HOME}/password.txt
 cat ~/password.txt | docker login --username ${DOCKER_USERNAME} --password-stdin
 
 docker push samjett/ou-reviews:latest
-docker push samjett/ou-reviews-api:latest
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -22,6 +21,3 @@ gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 # update
 gcloud compute instances update-container ou-reviews \
        --container-image docker.io/samjett/ou-reviews:latest
-
-gcloud compute instances update-container api-server \
-       --container-image docker.io/samjett/ou-reviews-api:latest
