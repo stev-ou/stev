4bceec4e485a89a0abf5fec518997b6836190fd2
Zach Schuermann <zachary.zvs@gmail.com>
2019-05-18 18:04:32 -0500

    added contrib component and contributing.md

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,22 +1,70 @@
 # Contributing to STEV
 
+Thank you for considering making contributions to the project! A brief outline
+follows, in order to provide a brief foundation for contributions. If you have
+any questions, feel free to drop us a line at
+[contact@evals.info](mailto:contact@evals.info).
+
+
 #### Table of Contents
 
-[Guidelines](#guidelines)
-
-[Getting Started](#getting-started)
-  * [](#)
-  * [](#)
-
-[Contributing](#contributing)
+**Contributing***
   * [Bug Reporting](#bug-reporting)
   * [Suggesting Improvements](#suggesting-improvements)
   * [Pull Requests](#pull-requests)
+  
+**Getting Started**
+  * [Getting Started](#getting-started)
+  * [Design Decisions](#design-decisions)
 
-## Guidelines
+## Bug Reporting
+Our bug reports are handled via GitHub issues. Upon discovering a bug, open an
+issue with the tag "bug" and provide the steps to reproduce. 
+
+## Suggesting Improvements
+Similar to bug reports, suggesting improvements or new features are also handled
+via GitHub. In order to file a suggestion/enhancement, open an issue in GitHub
+and assign the tag "feature request". 
 
 ## Getting Started
+The most involved form of contribution is adding to our source. If you have
+sufficient experience to make meaningful additions, we would love to have your
+help. This sort of contribution usually manifests in a pull request. In order to
+successfully modify our source and complete a pull request, you must first fork,
+then clone this repository. After obtaining a local copy of the source, follow
+instructions in the README to build the application. Upon successfully building
+the application, you can branch from master to implement a new feature or bug
+fix and then open a pull request upon completion! 
 
 ## Design Decisions
+In order to provide a minimal foundation for understanding the source, here lies
+a brief overview of the design of the application. The two main repositories are
+`stev` and `stev-api`, which house the frontend and backend portions of our
+application, respectively. The overall design is as follows: 
+```
+Mongo Atlas        Container         Container
++---------+       +---------+ HTTP  +----------+ HTTP 
+| MongoDB | <---> | Backend | <---> | Frontend | ---> Users
++---------+       +---------+  API  +----------+
+```
+The frontend is a React application which relies heavily on Redux and
+React-Router. The majority of the frontend is written in ES6 and linted with a
+utility called `prettier`. 
 
-## 
+The backend is a Python service which creates a WSGI web server hosted behind
+Gunicorn. 
+
+In production, the frontend is bundled using Webpack and then served behind an
+Nginx reverse-proxy. This is all containerized and the nginx/react app are
+served from Google Cloud Run simply running an instance (or multiple instances)
+of the frontend container. The backend is similarly packaged in a Docker image
+(using a Python base image) and run using Google Cloud Run. 
+
+## Pull Requests
+Feature branches should branch from master. After implementing functionality,
+open a pull request and our team will review the request. The PR must pass our
+CI/CD pipeline in order to be merged into master. In order to prevent
+unnecessary iterations, ensure all tests have run locally such that other
+functionality can be debugged upon opening the PR. If a number of contributions
+come from a single individual, our admins will gladly make you a mantainer of
+the project if interested. 
--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 
+/*
 class GetInvolved extends React.Component {
   constructor(props) {
     super(props);
@@ -17,5 +18,35 @@ class GetInvolved extends React.Component {
     );
   }
 }
+
+<li><b> :</b> Poll for student opinion in an effort to gather feedback and observe usage patterns. Take Student Name, ID and an "I approve" box?Captcha?Enforce 1 submission/IP?</li>
+*/
+
+const GetInvolved = props => (
+  <div className="container">
+    <div className="dataview-container">
+      <h1 style={{marginBottom: ".75em"}}><b>Get Involved</b></h1>
+      <p>Thanks for your interest in contributing to STEV! As this project continues to grow, there are increasingly many opportunities for involvement. Those with engineering backgrounds are encouraged to jump into the code and submit a pull request (more on that <a href="#pull-requests">below</a>). No matter your background, there are a number of ways to contribute to the project. A few recent challenges are listed here. </p>
+      <h3>Current Contribution Suggestions</h3>
+      <p><i>Updated Spring 2019</i></p>
+      <ul>
+        <li><b>Bug Tracking:</b> Submit a bug you discovered! Or submit an issue regarding the lack of data. If you discover a bug, make sure to jot down the steps to reproduce and submit a bug report in the "Issues" tab on our <a href="https://github.com/stev-ou">GitHub</a>. The latter contribution is especially valuable since our team has attempted to work with the university many times over, with very little receptiveness or action on the university's behalf. As we collect more information about missing data, and the demand for rectifying it, we will continue our attempts to gather more data from the university.</li>
+        <li><b>Suggesting Improvements:</b> After playing with the application, we encourage you to submit an issue on our <a href="https://github.com/stev-ou">GitHub</a> with the tag "Feature Request", where developers will be able to discuss and hopefully implement your request!</li>
+        <li><b>Source Code Contributions:</b> All source code is available online, hosted on <a href="https://github.com/stev-ou">GitHub</a>. We also use GitHub for bug tracking, issues, and suggested improvements as stated above. For example, browse the issues for the frontend repository <a href="https://github.com/stev-ou/stev/issues">here</a>. For those with software development experience, we would love your help. Our current team is small and we graciously welcome source contributions! For more information, see "Pull Requests" below.</li>
+      </ul>
+      <h3 id="pull-requests">Pull Requests</h3>
+      <p>Our source code is hosted on GitHub, in the organization <a href="https://github.com/stev-ou">stev-ou</a>. In order to make contributions, first fork and clone our repository to your local machine and build the site. The build instructions are included in the <a href="https://github.com/stev-ou/stev/blob/master/README.md">README</a> of each repository. Please consult the <a href="https://github.com/stev-ou/stev/blob/master/CONTRIBUTING.md">CONTRIBUTING.md</a> file for actionable details regarding contributing to our source. After creating a branch including your features/bug fixes, open a pull request for review by our team. After passing the review and our CI/CD pipeline, your contributions will be included in our source! If there are a number of contributions from an individual, we will happily welcome you as a maintainer of the project if interested.</p>
+      <h3>Contact</h3>
+      Reach our team for questions at <a href="mailto:contact@evals.info">contact@evals.info</a>.
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    </div>
+  </div>
+);
 
 export default GetInvolved;
