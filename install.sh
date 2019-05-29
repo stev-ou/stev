ff5fe5ec6c5bb28b38b6de22f9c1918b7b5e72cc
Zach Schuermann <zachary.zvs@gmail.com>
2019-05-29 10:26:45 -0500

    updated contrib and readme, and addressed libpng build error

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 command -v node >/dev/null 2>&1 || { echo >&2 "Nodejs is not installed.  Aborting."; exit 1; }
 command -v npm >/dev/null 2>&1 || { echo >&2 "NPM is not installed.  Aborting."; exit 1; }
 
-echo "Installing required packages..."
+echo "Checking system-level dependencies..."
+#sudo apt install -y gcc make libpng-dev
+command -v gcc >/dev/null 2>&1 || { echo >&2 "GCC is not installed.  Aborting."; exit 1; }
+command -v make >/dev/null 2>&1 || { echo >&2 "Make is not installed.  Aborting."; exit 1; }
+
+echo "Installing required npm packages..."
 npm install
