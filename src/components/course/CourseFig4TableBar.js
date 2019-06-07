efb5df81a024b4cab00883bb50a9172bb821c4d9
Sam Jett <samjett247@gmail.com>
2019-06-07 00:26:33 -0500

    Fixed shading issue in Question Numbers

--- src/components/course/CourseFig4TableBar.js
+++ src/components/course/CourseFig4TableBar.js
@@ -7,6 +7,26 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import obj from '../MobileTools.js';
 
 var question_colors = schemeSet3.slice(2);
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
 // Define mobile parameters
 var em = obj['em'];
 var mobile = obj['mobile'];
@@ -87,7 +107,7 @@ class CourseFig4TableBar extends React.Component {
           backgroundColor: question_colors[j],
           borderColor: 'rgba(255,255,255,1)',
           borderWidth: 1,
-          hoverBackgroundColor: question_colors[j + 1],
+          hoverBackgroundColor: shadeColor(question_colors[j], -10),
           hidden: !display_questions[j],
           hoverBorderColor: 'rgba(255,255,255,1)',
           data: result.questions[j].ratings.map(function(each_element) {
